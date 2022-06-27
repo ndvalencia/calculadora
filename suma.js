a79105335e020e3f82d311e184d96cbd2c037e97
Norman Valencia <nvalencia@livevox.com>
2022-06-27 18:32:13 -0500

    Mejoras de funcion suma

--- suma.js
+++ suma.js
@@ -1,6 +1,8 @@
-function sumar() {
-  var num1 = 5;
-  var num2 = 8;
+var num1 = 5;
+var num2 = 8;
+sumar(num1, num2);
+
+function sumar(num1, num2) {
   var suma = num1 + num2;
   alert("La suma es: " + suma);
 }
