30efdd01e617031916f75f142c2cf0a1ee35d52b
Norman Valencia <nvalencia@livevox.com>
2022-06-28 18:01:43 -0500

    Ajuste desde VSCode

--- suma.js
+++ suma.js
@@ -1,7 +1,7 @@
 var num1 = 5;
 var num2 = 8;
 var resultado = sumar(num1, num2);
-alert("La suma es: " + resultado);
+alert("El resultado de la suma es: " + resultado);
 
 function sumar(num1, num2) {
   return num1 + num2;
